3992f8fd67f35392afca6fe3c8cb9e1048594663
Roman Makliuk <voobragariym@gmail.com>
2021-09-14 18:05:45 +0300

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,5 @@
-# output "zookeeper_connect_string" {
-#   description = "Plaintext zookeeper host:port pairs"
-#   value = module.msk-main.zookeeper_connect_string
+
+
+# output "all_resources" {
+#   value = { for a in keys(var.alarms) : a => module.cloudwatch[a] }
 # }
-
-# output "bootstrap_brokers" {
-#   description = "Plaintext connection host:port pairs"
-#   value       = module.msk-main.bootstrap_brokers
-# }
-
-# output "bootstrap_brokers_tls" {
-#   description = "TLS connection host:port pairs"
-#   value       = module.msk-main.bootstrap_brokers_tls
-# }
-
-
-
-
-# output "alarm_sns_topic_arns" {
-#   description = "TLS connection host:port pairs"
-#   value       = aws_sns_topic.kafka_alerts_topic.arn
-# }
-
-# output "chatbot_slack_arns" {
-#   value       = module.chatbot.slack_arns
-# }
-
-# output "chatbot_role_arn" {
-#   value       = module.chatbot.role_arn
-# }
-
-# output "chatbot_role_name" {
-#   value       = module.chatbot.role_name
-# }
-
-
-# output "alarms" {
-#     value = [for a in module.cloudwatch: a.aws_cloudwatch_metric_alarm.alarms.arn]
-# }
-
-
-output "all_resources" {
-  value = { for a in keys(var.alarms) : a => module.cloudwatch[a] }
-}
