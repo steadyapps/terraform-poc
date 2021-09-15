6dbb75cb5c151ff9873ef3d73c75182b651496a4
zarat <igor.katrichenko@gmail.com>
2021-09-15 18:05:24 +0300

    msk

--- modules/msk/network.tf
+++ modules/msk/network.tf
@@ -10,16 +10,28 @@ resource "aws_subnet" "subnet_az1" {
   availability_zone = data.aws_availability_zones.main.names[0]
   cidr_block        = cidrsubnet(data.aws_vpc.main.cidr_block, 8, var.net_offset + 0)
   vpc_id            = data.aws_vpc.main.id
+  tags = {
+    Name        = "Subnet az1 for ${var.cluster_name}-${var.environment}"
+    Environment = var.environment
+  }
 }
 
 resource "aws_subnet" "subnet_az2" {
   availability_zone = data.aws_availability_zones.main.names[1]
   cidr_block        = cidrsubnet(data.aws_vpc.main.cidr_block, 8, var.net_offset + 1)
   vpc_id            = data.aws_vpc.main.id
+  tags = {
+    Name        = "Subnet az2 for ${var.cluster_name}-${var.environment}"
+    Environment = var.environment
+  }
 }
 
 resource "aws_subnet" "subnet_az3" {
   availability_zone = data.aws_availability_zones.main.names[2]
   cidr_block        = cidrsubnet(data.aws_vpc.main.cidr_block, 8, var.net_offset + 2)
   vpc_id            = data.aws_vpc.main.id
+  tags = {
+    Name        = "Subnet az3 for ${var.cluster_name}-${var.environment}"
+    Environment = var.environment
+  }
 }
